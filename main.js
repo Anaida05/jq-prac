fe723d7b8e6bae4da579c02e80c98902d6f2e1ca
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-08-05 17:47:49 +0530

    update

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
-$(document).ready(function(){
-    let btn = $("<button>Click</button>")
-    btn.on("click",function(){
-        $("#main").find("p").remove()
-    })
+// $(document).ready(function(){
+//     let btn = $("<button>Click</button>")
+//     btn.on("click",function(){
+//         $("#main").find("p").remove()
+//     })
 
-    $("#main").append(btn)
-    $("#main").prepend("<h2>hello world</h2>")
+//     $("#main").append(btn)
+//     $("#main").prepend("<h2>hello world</h2>")
 
-    let clone = $("<button>Clone Text</button>")
-    clone.on("click",function(){
-       let copy = $(".text").clone(true)
-       $("#main").append(copy)
-    })
-    $("#main").append(clone)
+//     let clone = $("<button>Clone Text</button>")
+//     clone.on("click",function(){
+//        let copy = $(".text").clone(true)
+//        $("#main").append(copy)
+//     })
+//     $("#main").append(clone)
 
 
-    let newSpan = $("<span>New Span</span>")
+//     let newSpan = $("<span>New Span</span>")
 
-    $("#main").append(newSpan)
+//     $("#main").append(newSpan)
 
-    // $("span").remove()
-    // $("#main > span").remove()
-    // $(newSpan).remove()
+//     // $("span").remove()
+//     // $("#main > span").remove()
+//     // $(newSpan).remove()
 
-    // $("span").css({
-    //     color : "red",
-    //     height : "20px",
-    //     width : "20px",
-    //     background: "orange"
-    // })
+//     // $("span").css({
+//     //     color : "red",
+//     //     height : "20px",
+//     //     width : "20px",
+//     //     background: "orange"
+//     // })
 
-    // $(newSpan).attr("class","orange")
-    // $(newSpan).attr("style","color : orange; font-size : 18px")
+//     // $(newSpan).attr("class","orange")
+//     // $(newSpan).attr("style","color : orange; font-size : 18px")
 
     
+// })
+
+
+$(document).ready(function(){
+    // let text = $("<p>Hello world</p>")
+    // $("#main").append(text)
+
+    // $("#main").text("Hii <b>goodmorninng</b>")
+    // $("#main").html("Hii <b>goodmorninng</b>")
+
+ $('#myCheck').prop('checked'); 
+  $('#myCheck').attr('checked'); 
 })
